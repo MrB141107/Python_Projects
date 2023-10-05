f415a5f2026ddea63840f6540fa92e26d7deee64
Jishnudeep Borah <85721902+Jishnu2608@users.noreply.github.com>
2023-10-05 22:46:28 +0530

    Update tetris.py - added comments

--- tetris.py
+++ tetris.py
@@ -1,3 +1,5 @@
+#pip install pygame
+
 import sys
 import pygame
 import random
@@ -6,9 +8,9 @@ pygame.init()
 
 # Screen dimensions
 WIDTH, HEIGHT = 800, 600
-GRID_SIZE = 25
+GRID_SIZE = 25 #size of each grid cell
 
-# Colors
+# Colors as defined by rgb values
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -16,7 +18,9 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 COLORS = [RED, BLUE, GREEN]
 
-# Tetromino shapes
+# Tetromino shapes - it represents the shapes of the tetris - "J", "L" , "I", "O", "S", "T", "Z". 
+# The '0' represents a filled cell and '.' represents an empty cell within the Tetromino piece. 
+# The pieces can be rotated and moved within the game grid. 
 SHAPES = [
     [
         ['.....',
@@ -99,24 +103,26 @@ SHAPES = [
     ],
 ]
 
-
+#Tetromino class to represent individual pieces
 class Tetromino:
     def __init__(self, x, y, shape):
+        #Initialize tetromino properties
         self.x = x
         self.y = y
         self.shape = shape
         self.color = random.choice(COLORS)  # You can choose different colors for each shape
         self.rotation = 0
 
-
+#tetris class to manage the game
 class Tetris:
     def __init__(self, width, height):
+        #intitialize the tetris game
         self.width = width
         self.height = height
         self.grid = [[0 for _ in range(width)] for _ in range(height)]
-        self.current_piece = self.new_piece()
+        self.current_piece = self.new_piece() #Intitialize first tetromino
         self.game_over = False
-        self.score = 0  # Add score attribute
+        self.score = 0  # Keep track of the player's score
 
     def new_piece(self):
         # Choose a random shape
@@ -124,8 +130,12 @@ class Tetris:
         # Return a new Tetromino object
         return Tetromino(self.width // 2, 0, shape)
 
+    
+    #Check if the current cell in the shape can be placed at a target position in the game grid
+    #Try catch block is to handle potential IndexErrors that may occur if the target position is out of bounds.
     def valid_move(self, piece, x, y, rotation):
         """Check if the piece can move to the given position"""
+        #Iterate over the cells of the Tetromino shape
         for i, row in enumerate(piece.shape[(piece.rotation + rotation) % len(piece.shape)]):
             for j, cell in enumerate(row):
                 try:
@@ -133,20 +143,25 @@ class Tetris:
                         return False
                 except IndexError:
                     return False
-        return True
+        return True #valid if all cells in the Tetromino shape can be placed in the grid
 
+    #Clear full lines in the game grid and return the number of lines cleared.
     def clear_lines(self):
         """Clear the lines that are full and return the number of cleared lines"""
+        # Iterate through the rows in the game grid, excluding the last row.
         lines_cleared = 0
         for i, row in enumerate(self.grid[:-1]):
             if all(cell != 0 for cell in row):
                 lines_cleared += 1
                 del self.grid[i]
-                self.grid.insert(0, [0 for _ in range(self.width)])
+                self.grid.insert(0, [0 for _ in range(self.width)]) #insert a new line at the top to replace the cleared line.
         return lines_cleared
 
+
+    #Lock the current piece in its current position and create a new piece to continue
     def lock_piece(self, piece):
         """Lock the piece in place and create a new piece"""
+        # Iterate over the cells of the Tetromino piece's shape in its current rotation.
         for i, row in enumerate(piece.shape[piece.rotation % len(piece.shape)]):
             for j, cell in enumerate(row):
                 if cell == 'O':
@@ -159,8 +174,9 @@ class Tetris:
         # Check if the game is over
         if not self.valid_move(self.current_piece, 0, 0, 0):
             self.game_over = True
-        return lines_cleared
+        return lines_cleared #return number of lines cleared in this move.
 
+    #Move current Tetromino piece down by one cell if game is not over.
     def update(self):
         """Move the tetromino down one cell"""
         if not self.game_over:
@@ -169,37 +185,43 @@ class Tetris:
             else:
                 self.lock_piece(self.current_piece)
 
+    #Draw game grid and the current Tetromino piece on the game screen.
     def draw(self, screen):
         """Draw the grid and the current piece"""
         for y, row in enumerate(self.grid):
             for x, cell in enumerate(row):
                 if cell:
+
+                    #draw a rectangle representing the filled cell at appropriate position and size.
                     pygame.draw.rect(screen, cell, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1))
 
+        #If there is current piece (game is not over), draw it on game screen.
         if self.current_piece:
             for i, row in enumerate(
                     self.current_piece.shape[self.current_piece.rotation % len(self.current_piece.shape)]):
                 for j, cell in enumerate(row):
                     if cell == 'O':
+
+                        #draw a rectangle representing the filled cell at appropriate position and size.
                         pygame.draw.rect(screen, self.current_piece.color, (
                         (self.current_piece.x + j) * GRID_SIZE, (self.current_piece.y + i) * GRID_SIZE, GRID_SIZE - 1,
                         GRID_SIZE - 1))
 
-
+#Function to draw the score with the given font
 def draw_score(screen, score, x, y):
     """Draw the score on the screen"""
     font = pygame.font.Font(None, 36)
     text = font.render(f"Score: {score}", True, WHITE)
     screen.blit(text, (x, y))
 
-
+#Function to draw game over with the given font
 def draw_game_over(screen, x, y):
     """Draw the game over text on the screen"""
     font = pygame.font.Font(None, 48)
     text = font.render("Game Over", True, RED)
     screen.blit(text, (x, y))
 
-
+#Main function
 def main():
     # Initialize pygame
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -209,7 +231,7 @@ def main():
     # Create a Tetris object
     game = Tetris(WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE)
     fall_time = 0
-    fall_speed = 50  # You can adjust this value to change the falling speed, it's in milliseconds
+    fall_speed = 50  #Adjust this value to change the falling speed, it's in milliseconds
     while True:
         # Fill the screen with black
         screen.fill(BLACK)
@@ -262,6 +284,6 @@ def main():
         # Set the framerate
         clock.tick(60)
 
-
+#Call main function to start the Tetris game when this script is executed.
 if __name__ == "__main__":
     main()
